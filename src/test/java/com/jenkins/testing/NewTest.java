035f3e3cf4bf7c6c1a6090a1e38aa414871875e6
Muhammad Sohaib <msohaib980@gmail.com>
2016-12-30 11:50:13 -0500

    Update NewTest.java

--- src/test/java/com/jenkins/testing/NewTest.java
+++ src/test/java/com/jenkins/testing/NewTest.java
@@ -12,11 +12,13 @@ public class NewTest {
   public void f() throws InterruptedException {
 	  
 	  WebDriver driver = new FirefoxDriver();
-		driver.get("https://msohaibdevvm1.dev.sagedining.com/intranet");
-		driver.findElement(By.id("authenticateUid")).sendKeys("msohaib");
-		driver.findElement(By.id("authenticatePw")).sendKeys("@College1");
-		driver.findElement(By.id("authenticateSubmit")).click();
-		Thread.sleep(5000);
+		// driver.get("https://msohaibdevvm1.dev.sagedining.com/intranet");
+		// driver.findElement(By.id("authenticateUid")).sendKeys("msohaib");
+		// driver.findElement(By.id("authenticatePw")).sendKeys("@College1");
+		 //driver.findElement(By.id("authenticateSubmit")).click();
+		//Thread.sleep(5000);
+	  String appUrl = "https://accounts.google.com";
+	                driver.get(appUrl);
 		
 	  
 	                
