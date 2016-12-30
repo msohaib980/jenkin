c510e6465e6fcfefd4dc8ec539b9dcbdc6c05716
muhamd sohaib <msohaib980@gmail.com>
2016-12-30 11:44:10 -0500

    dsfasdf

--- src/test/java/com/jenkins/testing/NewTest.java
+++ src/test/java/com/jenkins/testing/NewTest.java
@@ -1,5 +1,6 @@
 package com.jenkins.testing;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -8,12 +9,19 @@ import org.testng.annotations.Test;
 public class NewTest {
   @SuppressWarnings("unused")
 @Test
-  public void f() {
+  public void f() throws InterruptedException {
 	  
 	  WebDriver driver = new FirefoxDriver();
-	                String appUrl = "https://accounts.google.com";
-	                driver.get(appUrl);
-	 
+		driver.get("https://msohaibdevvm1.dev.sagedining.com/intranet");
+		driver.findElement(By.id("authenticateUid")).sendKeys("msohaib");
+		driver.findElement(By.id("authenticatePw")).sendKeys("@College1");
+		driver.findElement(By.id("authenticateSubmit")).click();
+		Thread.sleep(5000);
+		
+	  
+	                
+	                System.out.println("sadfsdf");
+	                driver.quit();
 	 
   }
 }
