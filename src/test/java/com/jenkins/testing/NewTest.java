5338bebbde1d7a9aed24035103157ca8c4633716
Muhammad Sohaib <msohaib980@gmail.com>
2017-01-11 14:33:12 -0500

    Update NewTest.java

--- src/test/java/com/jenkins/testing/NewTest.java
+++ src/test/java/com/jenkins/testing/NewTest.java
@@ -8,22 +8,40 @@ import org.testng.annotations.Test;
 
 public class NewTest {
   @SuppressWarnings("unused")
+  @BeforeClass
+  public void beforeClas() throws InterruptedException{
+	  
+	  WebDriver driver = 
+			  new FirefoxDriver();
+	  driver.get("https://www.sagedining.com/intranet/apps/messages/?p=123027");
+		driver.findElement(By.id("authenticateUid")).sendKeys("msohaib");
+		driver.findElement(By.id("authenticatePw")).sendKeys("@College12");
+		driver.findElement(By.id("authenticateSubmit")).click();
+		System.out.println(driver.getTitle());
+		System.err.println(driver.getWindowHandle());
+		Thread.sleep(500);
+	
+//		driver.findElement(By.xpath(".//*[@id='content']/div[5]/div/div/div[1]/div[2]/div[2]/div/div[1]/h2")).click();
+//		driver.findElement(By.tagName(name))
+		
+	  
+  }
+  
 @Test
   public void f() throws InterruptedException {
 	  
-	  WebDriver driver = new FirefoxDriver();
-		// driver.get("https://msohaibdevvm1.dev.sagedining.com/intranet");
-		// driver.findElement(By.id("authenticateUid")).sendKeys("msohaib");
-		// driver.findElement(By.id("authenticatePw")).sendKeys("@College1");
-		 //driver.findElement(By.id("authenticateSubmit")).click();
-		//Thread.sleep(5000);
-	  String appUrl = "https://msohaibdevvm1.dev.sagedining.com/intranet";
-	                driver.get(appUrl);
+//	driver.findElement(By.className("_1mf7 _4jy0 _4jy3 _4jy1 _51sy selected _42ft")).click();
+		
+//	WebDriver driver2 = new FirefoxDriver();
+//	driver2.get("https://www.sagedining.com/intranet/apps/messages/?p=123027");
+//			
+//		Assert.assertEquals(2, 3);
+		
 		
 	  
 	                
 	                System.out.println("sadfsdf");
-	                driver.quit();
+//	                driver.quit();
 	 
   }
 }
